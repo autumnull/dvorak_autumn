f321b697120e27e2841673929161b6936059ca5f
Autumn! <autumnull@protonmail.com>
2021-06-29 13:38:39 +0100

    deleted converter.py (it should never have been there)

--- converter.py
+++
@@ -1,9 +0,0 @@
-import json
-
-xorg_file = "/usr/share/X11/xkb/symbols/gb"
-keyboard_layout_online = "keyboard-layout.json"
-
-with open(xorg_file) as file:
-    data = json.load(file)
-
-print(data)
